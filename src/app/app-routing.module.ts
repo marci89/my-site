dc4f5bccff8e2de02f0b5d3f22b47f76be0761e4
marci <kismarczirobi@gmail.com>
2023-10-08 19:08:13 +0200

    Add components and create nav-bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { ReferenceComponent } from './components/reference/reference.component';
+import { CvComponent } from './components/cv/cv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'skill', component: SkillComponent },
+  { path: 'reference', component: ReferenceComponent },
+  { path: 'cv', component: CvComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { CvComponent } from './components/cv/cv.component';
+import { ReferenceComponent } from './components/reference/reference.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeaderComponent,
+    SkillComponent,
+    CvComponent,
+    ReferenceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

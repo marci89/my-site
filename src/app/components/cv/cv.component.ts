541cd9ca8e1332bab13a66d7f5f48a48a0caa406
marci <kismarczirobi@gmail.com>
2023-10-09 19:41:16 +0200

    pdf viewer and downloader

--- src/app/components/cv/cv.component.ts
+++ src/app/components/cv/cv.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
   styleUrls: ['./cv.component.css']
 })
-export class CvComponent {
+export class CvComponent  {
+  pdfUrl: SafeResourceUrl;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl('assets/pdf/Kismarczi_CV.pdf');
+  }
 }

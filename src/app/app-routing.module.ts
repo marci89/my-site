04dbd27a4275123c6916410aea3466fad3288661
marci <kismarczirobi@gmail.com>
2023-10-11 10:59:22 +0200

    skill page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,15 @@ import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skill', component: SkillComponent },
   { path: 'reference', component: ReferenceComponent },
   { path: 'cv', component: CvComponent },
   { path: 'experience', component: ExperienceComponent },
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  // Wildcard route for unmatched URLs
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({

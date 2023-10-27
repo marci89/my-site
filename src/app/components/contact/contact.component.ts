9ace1c25a0fb15cfda91ff0939c4551377103a6a
marci <kismarczirobi@gmail.com>
2023-10-27 07:55:33 +0200

    cat animation and css fix

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -6,8 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+    //freeze another sound when listening to cat voice
+    isSoundFreeze: boolean = false;
 
   locationUrl: string = "https://www.google.hu/maps/place/Szeged/@46.232789,20.1404686,11z/data=!3m1!4b1!4m6!3m5!1s0x474487e22bcce54b:0x400c4290c1e1190!8m2!3d46.2530102!4d20.1414253!16zL20vMDIzZzFo?entry=ttu";
 
   mailUrl: string = "mailto:kismarczirobi@gmail.com@?subject=feedback";
+
+  playCatVoice() {
+    if(!this.isSoundFreeze){
+    const catSound = new Audio('assets/sound/cat.mp3');
+    catSound.play();
+    this.isSoundFreeze = true;
+    setTimeout(() => {
+      this.isSoundFreeze = false;
+    }, 2000);
+    }
+  }
 }

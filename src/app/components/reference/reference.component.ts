9bdda30194e822d75b9b168a57e55364ab0ca7df
marci89 <kismarczirobi@gmail.com>
2024-02-03 14:38:29 +0100

    change references urls to github links

--- src/app/components/reference/reference.component.ts
+++ src/app/components/reference/reference.component.ts
@@ -18,12 +18,14 @@ export class ReferenceComponent implements OnInit{
     this.references = [
       {
         title: 'English Helper',
-        url: 'https://www.kismarczi.hu/english-helper/',
+       // url: 'https://www.kismarczi.hu/english-helper/',
+        url: 'https://github.com/marci89/EnglishHelper',
         image: 'assets/images/references/english-helper.png'
       },
       {
         title: 'KJ Brain Developer',
-        url: 'https://www.kismarczi.hu/kj-brain-developer/',
+      //  url: 'https://www.kismarczi.hu/kj-brain-developer/',
+        url: 'https://github.com/marci89/kj-brain-developer',
         image: 'assets/images/references/kj-brain-developer.png'
       }
     ]
